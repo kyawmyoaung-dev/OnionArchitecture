c71bc5896755afa76eb77ca1e523123a379d6fc7
Kyaw Myo Aung <info.kyawmyo@gmail.com>
2020-06-25 22:56:21 +0630

    Add project files.

---
+++ Application/Interfaces/IApplicationDbContext.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Microsoft.EntityFrameworkCore;
+using Domain.Entities;
+using System.Threading.Tasks;
+
+namespace Application.Interfaces
+{
+    public interface IApplicationDbContext
+    {
+        DbSet<Product> Products { get;set;}
+
+        Task<int> SaveChangesAsync();
+    }
+}
